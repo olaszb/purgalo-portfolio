064f8cc8ee1c78e73b3211de48d80e545f3fece7
gt47ob <gt47ob@inf.elte.hu>
2025-09-18 18:36:15 +0200

    Created and finished the projects section and added react-scroll cause css smooth scroll wasn't working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { cn } from "@/lib/utils";
 import { Menu, X } from "lucide-react";
 import { useEffect, useState } from "react";
+import { Link } from "react-scroll";
 
 const navItems = [
-  { name: "Home", href: "#hero" },
-  { name: "About", href: "#about" },
-  { name: "Skills", href: "#skills" },
-  { name: "Projects", href: "#projects" },
-  { name: "Contact", href: "#contact" },
+  { name: "Home", href: "hero" },
+  { name: "About", href: "about" },
+  { name: "Skills", href: "skills" },
+  { name: "Projects", href: "projects" },
+  { name: "Contact", href: "contact" },
 ];
 
 export const Navbar = () => {
@@ -31,30 +32,23 @@ export const Navbar = () => {
       )}
     >
       <div className="container flex items-center justify-between">
-        <a
-          className="text-xl font-bold text-primary flex items-center hover:text-foreground transition-colors duration-300"
-          href="#hero"
-        >
-          <span className="relative z-10 ">
-            <span className="text-glow text-foreground hover:text-primary transition-colors duration-300">
-              purgalo
-            </span>{" "}
-            <span className="hover:text-foreground transition-colors duration-300">
-              Portfolio
+        <Link to="hero" smooth={true} duration={500} className="text-xl font-bold text-primary flex items-center hover:text-foreground transition-colors duration-300">
+            <span className="relative z-10 space-x-1 cursor-pointer">
+              <span className="text-glow text-foreground hover:text-primary transition-colors duration-300">
+                Bálint
+              </span>
+              <span className="hover:text-foreground transition-colors duration-300">
+                Portfolio
+              </span>
             </span>
-          </span>
-        </a>
+        </Link>
 
         {/* desktop nav */}
         <div className="hidden md:flex space-x-8">
           {navItems.map((item, key) => (
-            <a
-              key={key}
-              href={item.href}
-              className="text-foreground/80 hover:text-primary transition-colors duration-300"
-            >
-              {item.name}
-            </a>
+            <Link to={item.href} smooth={true} duration={500} key={key} className="text-foreground/80 hover:text-primary transition-colors duration-300 cursor-pointer" >
+                {item.name}
+            </Link>
           ))}
         </div>
         {/* mobile nav */}
---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,104 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
+import { Element } from "react-scroll";
+
+const projects = [
+  {
+    id: 1,
+    title: "AI Meal Plan Generator",
+    description: "A meal plan generator app using AI, Next.js and TailwindCSS",
+    image: "/projects/aimealplan.png",
+    tags: ["Next.js", "TypeScript", "TailwindCSS"],
+    demoUrl: "https://aimealplan.store/",
+    githubUrl: "https://github.com/olaszb/ai-meal-plan",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    description: "A cosmic portfolio using React and TailwindCSS",
+    image: "/projects/portfolio.png",
+    tags: ["React", "JavaScript", "TailwindCSS"],
+    demoUrl: "#",
+    githubUrl: "https://github.com/olaszb/purgalo-portfolio",
+  },
+];
+
+export const ProjectsSection = () => {
+  return (
+    <Element name="projects">
+      <section id="projects" className="py-24 px-4 relative">
+        <div className="container mx-auto max-w-5xl">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+            Featured <span className="text-primary">Projects</span>
+          </h2>
+          <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+            Here are some of my recent projects. Each project was carefully
+            crafted with attention to detail, performance, and user experience.
+          </p>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {projects.map((project) => (
+              <div
+                key={project.id}
+                className="group bg-card rounded-lg overflow-hidder shadow-xs card-hover"
+              >
+                <div className="h-48 overflow-hidden">
+                  <img
+                    className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                    src={project.image}
+                    alt={project.title}
+                  />
+                </div>
+
+                <div className="p-6">
+                  <div className="flex flex-wrap gap-2 mb-4">
+                    {project.tags.map((tag, key) => (
+                      <span
+                        key={key}
+                        className="px-2 py-1 text-xs font-medium border rounded-full bg-primary/20 text-secondary-foreground"
+                      >
+                        {tag}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+
+                <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+                <p className="text-muted-foreground text-sm mb-4 mx-1">
+                  {project.description}
+                </p>
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-3 p-3">
+                    <a
+                      href={project.demoUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <ExternalLink size={20} />
+                    </a>
+                    <a
+                      href={project.githubUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <Github size={20} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="text-center mt-12">
+            <a
+              className="cosmic-button w-fit flex items-center mx-auto gap-2"
+              href="https://github.com/olaszb"
+              target="_blank"
+            >
+              Check Out My Github <ArrowRight size={16} />
+            </a>
+          </div>
+        </div>
+      </section>
+    </Element>
+  );
+};
