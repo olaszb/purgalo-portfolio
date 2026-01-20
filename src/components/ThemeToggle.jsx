02c07172bfda2bf7d989160b70b92334ff487fa0
purgalo <olasz.g48@gmail.com>
2026-01-20 17:19:16 +0100

    changed colors of light mode

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -38,9 +38,9 @@ export const ThemeToggle = () => {
       )}
     >
       {isDarkMode ? (
-        <Sun className="h-6 w-6 text-yellow-300" />
-      ) : (
         <Moon className="h-6 w-6 blue-900" />
+      ) : (
+        <Sun className="h-6 w-6 text-yellow-300" />
       )}
     </button>
   );
